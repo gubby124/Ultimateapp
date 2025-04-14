05a9538253c53c60ac2dfa5daa25f38954a4a77e
gubby124 <epc213@proton.me>
2025-04-13 19:52:11 -0500

    Update sounds.js

--- tabs/sounds.js
+++ tabs/sounds.js
@@ -1,13 +1,8 @@
 export function renderSounds() {
   const container = document.getElementById('sound');
-  const sounds = [
-    { label: "Pop", url: "https://www.fesliyanstudios.com/play-mp3/387" },
-    { label: "Click", url: "https://www.fesliyanstudios.com/play-mp3/3872" }
-  ];
-  sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.innerText = sound.label;
-    btn.onclick = () => new Audio(sound.url).play();
-    container.appendChild(btn);
-  });
+  container.innerHTML = "<h2>Sounds</h2>";
+  const btn = document.createElement('button');
+  btn.innerText = "Play Click";
+  btn.onclick = () => document.getElementById('click-sound').play();
+  container.appendChild(btn);
 }
